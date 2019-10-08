fa9b03c21886636922f622b7a8e11063b080faf6
Mirosław Buszek <zoowett@gmail.com>
2019-10-08 13:23:32 +0200

    Initial commit

---
+++ src/main/java/com/buszek/EmployeePermissionListApplication.java
@@ -0,0 +1,13 @@
+package com.buszek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePermissionListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePermissionListApplication.class, args);
+	}
+
+}

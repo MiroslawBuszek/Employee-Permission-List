a228f5f122edce3e3764f6b14cb6ac94698ebc22
Mirosław Buszek <zoowett@gmail.com>
2019-10-09 02:05:03 +0200

    Add user controller

--- src/main/java/com/buszek/controller/UserController.java
+++ src/main/java/com/buszek/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.buszek.controller;
 
 import com.buszek.LoginForm;
+import com.buszek.model.User;
 import com.buszek.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -13,41 +20,41 @@ public class UserController {
     private UserRepository repository;
 	
     @RequestMapping(value = "signup")
-    public String addStudent(Model model){
+    public String addEmployee(Model model){
     	model.addAttribute("signupform", new LoginForm());
         return "signup";
     }	
     
-//    @RequestMapping(value = "saveuser", method = RequestMethod.POST)
-//    public String save(@Valid @ModelAttribute("signupform") LoginForm loginForm, BindingResult bindingResult) {
-//    	System.out.println(bindingResult.toString());
-//    	if (!bindingResult.hasErrors()) { // validation errors
-//    		if (loginForm.getPassword().equals(loginForm.getPasswordCheck())) { // check password match
-////	    		String pwd = loginForm.getPassword();
-////		    	BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
-////		    	String hashPwd = bc.encode(pwd);
-//
-//		    	User newUser = new User();
-////		    	newUser.setPasswordHash(hashPwd);
-//		    	newUser.setUsername(loginForm.getUsername());
-//		    	newUser.setRole("USER");
-//		    	if (repository.findByUsername(loginForm.getUsername()) == null) {
-//		    		repository.save(newUser);
-//		    	}
-//		    	else {
-//	    			bindingResult.rejectValue("username", "error.userexists", "Username already exists");
-//	    			return "signup";
-//		    	}
-//    		}
-//    		else {
-//    			bindingResult.rejectValue("passwordCheck", "error.pwdmatch", "Passwords does not match");
-//    			return "signup";
-//    		}
-//    	}
-//    	else {
-//    		return "signup";
-//    	}
-//    	return "redirect:/login";
-//    }
+    @RequestMapping(value = "saveuser", method = RequestMethod.POST)
+    public String save(@Valid @ModelAttribute("signupform") LoginForm loginForm, BindingResult bindingResult) {
+    	System.out.println(bindingResult.toString());
+    	if (!bindingResult.hasErrors()) {
+    		if (loginForm.getPassword().equals(loginForm.getPasswordCheck())) {
+	    		String pwd = loginForm.getPassword();
+		    	BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+		    	String hashPwd = bc.encode(pwd);
+
+		    	User newUser = new User();
+		    	newUser.setPasswordHash(hashPwd);
+		    	newUser.setUsername(loginForm.getUsername());
+		    	newUser.setRole("USER");
+		    	if (repository.findByUsername(loginForm.getUsername()) == null) {
+		    		repository.save(newUser);
+		    	}
+		    	else {
+	    			bindingResult.rejectValue("username", "error.userexists", "Username already exists");
+	    			return "signup";
+		    	}
+    		}
+    		else {
+    			bindingResult.rejectValue("passwordCheck", "error.pwdmatch", "Passwords does not match");
+    			return "signup";
+    		}
+    	}
+    	else {
+    		return "signup";
+    	}
+    	return "redirect:/login";
+    }
     
 }

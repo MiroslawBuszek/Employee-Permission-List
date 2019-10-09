3cdf108971df41b6af5f501a73e08b36b3a33850
Mirosław Buszek <zoowett@gmail.com>
2019-10-09 21:39:13 +0200

    Refactor employee controller

--- src/main/java/com/buszek/controller/EmployeeController.java
+++ src/main/java/com/buszek/controller/EmployeeController.java
@@ -14,80 +14,80 @@ import java.util.Optional;
 
 @Controller
 public class EmployeeController {
-	@Autowired
+    @Autowired
     private EmployeeRepository emplRepository;
 
-	@Autowired
+    @Autowired
     private PermissionRepository permRepository;
-	
-	@RequestMapping("/login")
-	public String login() {
-    	return "login";
-    }	
-	
-	@RequestMapping("/employees")
-	public String index(Model model) {
-		List<Employee> employees = (List<Employee>) emplRepository.findAll();
-		model.addAttribute("employees", employees);
-    	return "employees";
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/employees")
+    public String index(Model model) {
+        List<Employee> employees = (List<Employee>) emplRepository.findAll();
+        model.addAttribute("employees", employees);
+        return "employees";
     }
 
     @RequestMapping(value = "add")
-    public String addEmployee(Model model){
-    	model.addAttribute("employee", new Employee());
+    public String addEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
         return "addEmployee";
-    }	
+    }
 
     @RequestMapping(value = "/edit/{id}")
-    public String editEmployee(@PathVariable("id") Long employeeId, Model model){
-    	model.addAttribute("employee", emplRepository.findById(employeeId));
+    public String editEmployee(@PathVariable("id") Long employeeId, Model model) {
+        model.addAttribute("employee", emplRepository.findById(employeeId));
         return "editEmployee";
-    }	    
-    
-    @RequestMapping(value = "save", method = RequestMethod.POST)
-    public String save(Employee employee){
-		emplRepository.save(employee);
-    	return "redirect:/employees";
     }
-    
+
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(Employee employee) {
+        emplRepository.save(employee);
+        return "redirect:/employees";
+    }
+
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteEmployee(@PathVariable("id") Long employeeId, Model model) {
-		emplRepository.deleteById(employeeId);
+        emplRepository.deleteById(employeeId);
         return "redirect:/employees";
-    }    
-    
-    @RequestMapping(value = "addEmployeePermission/{id}", method = RequestMethod.GET)
-    public String addPermission(@PathVariable("id") Long employeeId, Model model){
-
-    		model.addAttribute("permissions", permRepository.findAll());
-    		model.addAttribute("employee", emplRepository.findById(employeeId).get());
-    		return "addEmployeePermission";
     }
-    
-    
-    @RequestMapping(value="/employee/{id}/permissions", method= RequestMethod.GET)
-	public String employeesAddPermission(@RequestParam(value="action", required=true) String action, @PathVariable Long id, @RequestParam Long permissionId, Model model) {
-    	Optional<Permission> permisson = permRepository.findById(permissionId);
-		Optional<Employee> employee = emplRepository.findById(id);
 
-		if (employee.isPresent() && action.equalsIgnoreCase("save")) {
-			if (!employee.get().hasPermission(permisson.get())) {
-				employee.get().getPermissions().add(permisson.get());
-			}
-			emplRepository.save(employee.get());
-			model.addAttribute("employee", permRepository.findById(id));
-			model.addAttribute("permission", permRepository.findAll());
-			return "redirect:/employees";
-		}
+    @RequestMapping(value = "addEmployeePermission/{id}", method = RequestMethod.GET)
+    public String addPermission(@PathVariable("id") Long employeeId, Model model) {
 
-		model.addAttribute("developers", emplRepository.findAll());
-		return "redirect:/eemployees";
-		
-	}    
-    
+        model.addAttribute("permissions", permRepository.findAll());
+        model.addAttribute("employee", emplRepository.findById(employeeId).get());
+        return "addEmployeePermission";
+    }
+
+
+    @RequestMapping(value = "/employee/{id}/permissions", method = RequestMethod.GET)
+    public String employeesAddPermission(@RequestParam(value = "action", required = true) String action, @PathVariable Long id, @RequestParam Long permissionId, Model model) {
+        Optional<Permission> permisson = permRepository.findById(permissionId);
+        Optional<Employee> employee = emplRepository.findById(id);
+
+        if (employee.isPresent() && action.equalsIgnoreCase("save")) {
+            if (!employee.get().hasPermission(permisson.get())) {
+                employee.get().getPermissions().add(permisson.get());
+            }
+            emplRepository.save(employee.get());
+            model.addAttribute("employee", permRepository.findById(id));
+            model.addAttribute("permission", permRepository.findAll());
+            return "redirect:/employees";
+        }
+
+        model.addAttribute("developers", emplRepository.findAll());
+        return "redirect:/eemployees";
+
+    }
+
     @RequestMapping(value = "getemployees", method = RequestMethod.GET)
     public @ResponseBody
     List<Employee> getEmployees() {
-            return (List<Employee>)emplRepository.findAll();
-    }      
+        return (List<Employee>) emplRepository.findAll();
+    }
 }
